3482f542c68719303e212f2c29b5961f8c72290b
Omar <153245800+omar-vm@users.noreply.github.com>
2024-12-18 23:11:35 -0600

    cambios init

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,26 +1,33 @@
 from flask import Flask, jsonify
 from flask_restful import Api
-from flask_cors import CORS  # Importa CORS
+from flask_cors import CORS
 from .routes import api_bp, signup_bp
 from .config import Config
 from .extensions import db, jwt, migrate
 from flask_jwt_extended.exceptions import NoAuthorizationError, InvalidHeaderError
-from dotenv import load_dotenv
 
-def conf_app():
+
+def create_app():
     app = Flask(__name__)
+
+    # Carga configuración
     app.config.from_object(Config)
+
+    # Inicializa extensiones
     db.init_app(app)
     jwt.init_app(app)
     migrate.init_app(app, db)
-    CORS(app)  # Para permitir las solicitudes desde el frontend
+    CORS(app)
 
+    # Crea las tablas necesarias si no existen
     with app.app_context():
         db.create_all()
 
+    # Registra los Blueprints
     app.register_blueprint(api_bp)
     app.register_blueprint(signup_bp)
 
+    # Manejo de errores personalizados
     @app.errorhandler(NoAuthorizationError)
     def no_token_manage(error):
         return jsonify({'Mensaje': 'Necesitas un token para acceder', 'Error': str(error)}), 401
