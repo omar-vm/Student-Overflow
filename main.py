d4c6db06a0225ea7bf4d62909e1ff3cfe969f0bb
Omar <153245800+omar-vm@users.noreply.github.com>
2024-12-18 23:13:08 -0600

    cambios main

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-from backend import conf_app
+from backend import create_app  # Usa la instancia configurada en __init__.py
 from frontend.login import LoginWindow
 from PyQt6.QtWidgets import QApplication
 import threading
 import sys
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def run_flask_app():
-    app = conf_app()
-    app.run(port=5001, use_reloader=False)
+    app = create_app()  # Crea la instancia de Flask
+    app.run(port=5001, use_reloader=False)  # Corre Flask en el puerto 5001
 
 if __name__ == "__main__":
+    # Inicia el hilo de Flask
     flask_thread = threading.Thread(target=run_flask_app)
     flask_thread.daemon = True
     flask_thread.start()
 
+    # Inicia la interfaz gráfica con PyQt6
     qt_app = QApplication(sys.argv)
     login_window = LoginWindow()
     login_window.show()
